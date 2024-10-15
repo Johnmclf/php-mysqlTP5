f6539c6524c3e02b54054d4fcc9fef5b7c989fae
Johnmclf <150351096+Johnmclf@users.noreply.github.com>
2024-10-15 11:24:27 +0200

    Améliorations esthétiques

--- blog/index.php
+++ blog/index.php
@@ -2,11 +2,11 @@
 require('../src/model.php');
 $posts = getPosts();
 require('../templates/homepage.php');
-?>
 
-<!-- 
-Les 3 erreurs importantes sont :
-- Utilisation de n'est pas conseillé die / exemple utiliser catch 
-- Aucun filtre de sécuriter lors de l'ajout / exemple utiliser filter_var()
-- Généralement la connection à la base de données et effectuer avant le HTML (éviter de mélanger le php et l'HTML)
--->
+
+
+// Les 3 erreurs importantes sont :
+// - Utilisation de n'est pas conseillé die / exemple utiliser catch 
+// - Aucun filtre de sécuriter lors de l'ajout / exemple utiliser filter_var()
+// - Généralement la connection à la base de données et effectuer avant le HTML (éviter de mélanger le php et l'HTML)
+
